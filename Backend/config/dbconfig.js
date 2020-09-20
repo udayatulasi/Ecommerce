47e2eb4179a8b3c2b18fbbc0100aa77c58677e8f
Hemanth Babu Parupalli <hemanth@nuenergy.ai>
2020-09-20 03:35:57 -0300

    Added try/catch to dbconfig

--- Backend/config/dbconfig.js
+++ Backend/config/dbconfig.js
@@ -3,13 +3,24 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async() =>{
-    const conn = await mongoose.connect( process.env.MONGO_URI,{
-        useNewUrlParser : true,
-        useCreateIndex : true,
-        useFindAndModify : false,
-        useUnifiedTopology: true 
-    })
-    console.log(`DB connected to ${ conn.connection.host}`)
+
+    try{
+
+        const conn = await mongoose.connect( process.env.MONGO_URI,{
+            useNewUrlParser : true,
+            useCreateIndex : true,
+            useFindAndModify : false,
+            useUnifiedTopology: true 
+        })
+        console.log(`DB connected to ${ conn.connection.host}`)
+
+    }
+
+    catch{
+        console.log('please check your db')
+        
+    }
+    
 }
 
 module.exports = connectDB;
