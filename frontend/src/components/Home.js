72c1f1aa460d1519cbbd6dda02ccb32b09623653
udayatulasi <binnuuday05@gmail.com>
2020-10-07 11:50:35 -0300

    small chnages

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Body from '../components/Body';
-import Footer from "../components/Footer"
 import { Link } from 'react-router-dom'
-import Headers from './Header'
+import Headers from './Header/Header'
 import Footer from './Footer'
 import Footer1 from "./Footer1"
 import Footer2 from "./Footer2"
@@ -13,11 +12,10 @@ import TopCarousel from './landingPageTopCarousel/TopCarousel'
 export default function Home() {
     return (
         <div>
+        <Headers />
           <TopCarousel />
           <Body />
-          <Footer />
         <Scroll />
-        <Headers />
         <Footer1 />         
         <Footer2 />
         <Footer />
--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -3,11 +3,7 @@ import React from 'react'
 export default function Blog() {
     return (
         <div>
-<<<<<<< HEAD:frontend/src/pages/Blog.js
-            <h6>Blog</h6>
-=======
     
->>>>>>> ankath:frontend/src/components/Header.js
         </div>
     )
 }
